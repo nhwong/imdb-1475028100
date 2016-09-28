8c05a753bd9db81f96afb8a5cc39d51dea4b155e
Raghu Betina <raghu@firstdraft.com>
2016-09-28 02:04:35 +0000

    Generate movies resource with starter:resource

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,50 @@
+class MoviesController < ApplicationController
+  def index
+    @movies = Movie.all
+  end
+
+  def show
+    @movie = Movie.find(params[:id])
+  end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+    @movie = Movie.new
+    @movie.name = params[:name]
+    @movie.director = params[:director]
+
+    if @movie.save
+      redirect_to "/movies", :notice => "Movie created successfully."
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @movie = Movie.find(params[:id])
+  end
+
+  def update
+    @movie = Movie.find(params[:id])
+
+    @movie.name = params[:name]
+    @movie.director = params[:director]
+
+    if @movie.save
+      redirect_to "/movies", :notice => "Movie updated successfully."
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @movie = Movie.find(params[:id])
+
+    @movie.destroy
+
+    redirect_to "/movies", :notice => "Movie deleted."
+  end
+end
